f9f2df71a5b360f132512d6940d3745e9baa1376
Artem Kuznetsov <artem.kuznetsov@orioninc.com>
2022-05-26 15:00:15 +0300

    first variant of my test task

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.scss";
 
 interface ICourses {
@@ -7,10 +6,6 @@ interface ICourses {
 }
 
 function App() {
-  const [firstInputState, setFirstInputState] = useState("");
-  const [secondInputState, setSecondInputState] = useState("");
-  const [isEnterPressed, setIsEnterPressed] = useState(false);
-
   let courses: ICourses[] = [
     { name: "Courses in England", prices: [0, 100] },
     { name: "Courses in Germany", prices: [500, null] },
@@ -22,117 +17,59 @@ function App() {
     { name: "Courses in France", prices: [null, null] },
   ];
 
-  function checkFunc(firstPrice: string, secondPrice: string) {
-    const result: ICourses[] = courses.filter((item) => {
-      // First type
-      if (
-        (item.prices[0] <= Number(firstPrice) ||
-          item.prices[1] >= Number(firstPrice)) &&
-        item.prices[1] <= Number(secondPrice) &&
-        item.prices[0] <= Number(secondPrice) &&
-        firstPrice.length &&
-        secondPrice.length &&
-        item.prices[0] !== null &&
-        item.prices[1] !== null
-      ) {
-        return item;
+  function findCourses(pricesArray: number[]) {
+    const result = courses.filter((item) => {
+      // У данного решения есть баг. Если пользователь передает начальную цену выше конечной, то функция работает неверно.
+      // Однако такие ошибки обычно обрабатываются еще до этапа, когда вызывается фильтрующая функция. Поэтому внутри функции
+      // Эту ошибку не обрабатывал.
+
+      // Необходимо приравнять начальную цену существующих курсов (если null) к 0 для избежания проблем при сравнении
+      if (item.prices[0] === null) {
+        item.prices[0] = 0;
       }
-      // Fourth type
-      if (
-        !firstPrice.length &&
-        item.prices[0] === null &&
-        secondPrice.length &&
-        Number(secondPrice) >= item.prices[1] &&
-        item.prices[1] !== null
-      ) {
-        return item;
+      // Необходимо приравнять конечную цену существующих курсов(если null) к бесконечности, т.к. конечная цена может быть любой.
+      // Также необходимо для избежания проблем при сравнении
+      if (item.prices[1] === null) {
+        item.prices[1] = Infinity;
       }
-      // Third type
-      if (
-        !secondPrice.length &&
-        item.prices[1] === null &&
-        firstPrice.length &&
-        Number(firstPrice) >= item.prices[0] &&
-        item.prices[0] !== null
-      ) {
-        return item;
+      // Аналогично для начальной цены, которая передается при вызове функции
+      if (pricesArray[0] === null) {
+        pricesArray[0] = 0;
       }
-      // Fifth type
+      // Аналогично для конечной цены, которая передается при вызове функции
+      if (pricesArray[1] === null) {
+        pricesArray[1] = Infinity;
+      }
+      // В этой проверке аргумент функции сравнивается с курсами, у которых указана начал./конеч. стоимость, и у которых
+      // есть только конечная стоимость. Если добавлять сравнение с курсами без конечной стоимости, то функция выполнется некорректно.
       if (
-        !firstPrice.length &&
-        !secondPrice.length &&
-        item.prices[0] === null &&
-        item.prices[1] === null
+        pricesArray[1] >= item.prices[1] &&
+        pricesArray[1] >= item.prices[0] &&
+        item.prices[1] !== Infinity
       ) {
-        return item;
+        return true;
+      }
+      // В этой проверке аргумент функции сравнивается с курсами, у которой нет конечной цены. Здесь нам важно, чтобы конечная цена
+      // аргумента функции не была больше начальной в курсах.
+      if (item.prices[1] === Infinity && pricesArray[1] >= item.prices[0]) {
+        return true;
       }
       return false;
     });
-    if (!result.length) {
-      return <span>Sorry, but we don't have courses with such prices</span>;
-    }
-    return <span>{`Buy ${result.map((item) => item.name).join(", ")}`}</span>;
+
+    return result;
   }
 
-  return (
-    <div className="App">
-      <div className="header">
-        <h1>Welcome!</h1>
-        <h2>Сhoose the desired course cost</h2>
-      </div>
-      <div className="body">
-        <div className="first-input">
-          <span>From</span>
-          <input
-            type="number"
-            onChange={(e) => setFirstInputState(e.target.value)}
-            onKeyPress={(e) => {
-              if (e.key === "Enter") {
-                setIsEnterPressed(true);
-              }
-            }}
-            value={firstInputState}
-          />
-        </div>
-        <div className="second-input">
-          <span>To</span>
-          <input
-            type="number"
-            onChange={(e) => setSecondInputState(e.target.value)}
-            onKeyPress={(e) => {
-              if (e.key === "Enter") {
-                setIsEnterPressed(true);
-              }
-            }}
-            value={secondInputState}
-          />
-        </div>
-        <button
-          onClick={() => {
-            setIsEnterPressed(true);
-          }}
-        >
-          Find
-        </button>
-        <button
-          onClick={() => {
-            setIsEnterPressed(false);
-            setFirstInputState("");
-            setSecondInputState("");
-          }}
-        >
-          Reset
-        </button>
-      </div>
-      <div className="result">
-        {isEnterPressed ? (
-          checkFunc(firstInputState, secondInputState)
-        ) : (
-          <div></div>
-        )}
-      </div>
-    </div>
-  );
+  let requiredRange1 = [null, 200];
+  let requiredRange2 = [100, 350];
+  // При передачи requiredRange3 в аргумент функции, вернутся все курсы, поскольку конечная цена неуказана, а значит бюджет неограничен.
+  // Можно брать любые курсы.
+  let requiredRange3 = [200, null];
+  console.log(findCourses(requiredRange1));
+
+  // Курсы из Франции возвращаются всегда, т.к. здесь цена не имеет никакого значения, а значит сравнивать с чем-либо не надо.
+
+  return <div></div>;
 }
 
 export default App;
